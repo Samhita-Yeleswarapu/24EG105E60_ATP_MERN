bc09369b41447d13c58b98f4fb1e0ae37d4745da
Samhita Yeleswarapu <samhitayeleswarapu@gmail.com>
2026-05-06 10:33:39 +0530

    Create EmployeeModel with validation schema

---
+++ Week6/Mern_Mini_App/backend3/models/EmployeeModel.js
@@ -0,0 +1,49 @@
+//create user schema
+import { Schema, model } from "mongoose";
+
+//create employee schema
+const employeeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email"]
+    },
+
+    mobile: {
+      type: String,
+      required: [true, "Mobile number is required"],
+      unique: true,
+      match: [/^[0-9]{10}$/, "Mobile number must be 10 digits"]
+    },
+
+    designation: {
+      type: String,
+      required: [true, "Designation is required"],
+      trim: true
+    },
+
+    companyName: {
+      type: String,
+      required: [true, "Company name is required"],
+      trim: true
+    }
+  },
+  {
+    strict: "throw",
+    versionKey: false,
+    timestamps: true
+  }
+);
+
+//generate EmployeeModel
+export const EmployeeModel = model("Employee", employeeSchema);
