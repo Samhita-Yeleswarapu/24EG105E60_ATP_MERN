6637ddaabbf2902ca3a311b30e9d35fb905b04ad
Samhita Yeleswarapu <samhitayeleswarapu@gmail.com>
2026-05-19 14:03:49 +0530

    Implement task addition and retrieval in app.js
    Added task management functionality to app.js.

---
+++ Week2/task-management/app.js
@@ -0,0 +1,10 @@
+import {addTask,getAllTasks} from './task.js';
+// Test your module system
+// 1. Add some tasks
+addTask("eating","High",'2024-12-12')
+addTask("sleeping","High",'2024-12-12')
+// 2. Display all tasks
+const tasks=getAllTasks()
+console.log(tasks)
+// 3. Complete a task
+// 4. Display all tasks again
